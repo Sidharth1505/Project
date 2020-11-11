4086bff606066529a4941e1d9186083e96230685
Sidharth Jain <sidharthjain.1505@gmail.com>
2020-11-12 02:41:58 +0530

    Project Started

---
+++ Prashan_Uttar/Prashan_Uttar/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Prashan_Uttar project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prashan_Uttar.settings')
+
+application = get_asgi_application()
